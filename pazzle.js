9f847f51229c1b4eae7294c5a5205914649c56e4
Alina <ananyivaalina@gmail.com>
2015-05-18 19:53:09 +0300

    Update pazzle.js

--- pazzle.js
+++ pazzle.js
@@ -1,42 +1,28 @@
 $(document).ready(function(){
-//Переменную cellSize не менять!!! Не присваивать ей ноль.
-var cellSize = 50, //Размер ячейки.
-    imgSize = 500, //Размер изображения.
-	rowAmountConst = imgSize/cellSize, //Количество колонок. Будет задано одинаковое количество колонок и рядов.
-	rowAmount = 0,//Количество колонок для счётчика, чтоб не менять основную переменную.
-	currentRow = 0,//Текущий ряд.
-	cellAmount = rowAmountConst*10, //Общее количество ячеек.
-	horizontalIndent = 0,//Горизонтальный отступ.
-	verticalIndent = 0;//Вертикальный отступ.
+//Don`t set cellSize to zero!
+var cellSize = 50, 
+    imgSize = 500,
+	rowAmountConst = imgSize/cellSize, //Row amount will be equal to column amount.
+	rowAmount = 0,//Row amount for counter. It will help me to avoid changing the main variable.
+	currentRow = 0,
+	cellAmount = rowAmountConst*10, //General cell amount.
+	horizontalIndent = 0,
+	verticalIndent = 0;
 	
-	function getXPositionOfElement() {//Получить случайные координаты x окна браузера.
-        var x_position = Math.floor(Math.random() * $( "#pazzleHolder" ).width());
-	    return x_position;
-    }
-	
-	function getYPositionOfElement() {//Получить случайные координаты y окна браузера.
-        var y_position = Math.floor(Math.random() * $( "#pazzleHolder" ).height());
-	    return y_position;
-    }
-	
-	
-	
-	//$(".pazzlePartHolder").css({"width":cellSize+"px", "height":cellSize+"px"});//Функция работает некорректно.
-    $("body").append("<div id='pazzleHolder'>");//Открыть div, содержащий весь пазл.
-    for(var currentCell=0; currentCell<cellAmount; currentCell++){//Пока, текущая ячейка меньше общего количества ячеек.
-	    $("div#pazzleHolder").append("<div id="+currentCell+" class='pazzlePartHolder draggable'></div>");//Добавить блок, содержащий часть пазла.
-        $("div#"+currentCell).append("<img src='wallpaper.jpg' class='"+"block"+currentCell+"' alt='img' />");//В этот блок добавить img и присвоить уникальный класс "block"+i+.
-		if(currentCell==0){//Если отображается первая ячейка, то задать ей нулевое смещение вверх. 
+ $("body").append("<div id='pazzleHolder'>");//Open div that contains all puzzle..
+    for(var currentCell=0; currentCell<cellAmount; currentCell++){//While current cell is less then general cell amount.
+	    $("div#pazzleHolder").append("<div id="+currentCell+" class='pazzlePartHolder draggable'></div>");//Adds div that contains the part of puzzle..
+        $("div#"+currentCell).append("<img src='wallpaper.jpg' class='"+"block"+currentCell+"' alt='img' />");//Let`s add img and the unique class "block"+i+.
+		if(currentCell==0){//If there is the first cell, then it will have zero top indent.
 		    $(".block"+currentCell).css({'position': 'absolute','top' : 0+"px",'left' : 0+"px", 'padding':'0px'});
-		}else if(currentCell==cellAmount)break;//Если цикл добрался до последней ячейки - завершить итерации.
+		}else if(currentCell==cellAmount)break;//If the is the last iteration, then break.
 		if(currentCell==rowAmount){
-		    rowAmount+=rowAmountConst;//Если текущая ячейка равна концу ряда, то увеличить её на количество ячеек в ряду. Чтоб можно было сравнивать с крайними числами (например: 3, 6, 9, 12, 16).
-			horizontalIndent=horizontalIndent-cellSize;//Сдвинуть координаты отступа (смещения) left.
-			verticalIndent = 0;//...и top. Сдвинуть координаты в начало следующего столбца.
-			$(currentCell).addClass("topOfTheRow");//Присвоить класс текущему элементу, благодаря чему, в стилях можно будет задать display: inline-block;, для данного класса.
+		    rowAmount+=rowAmountConst;//If current cell equals the end of row, then let`s increment it to row amount in the row. So, we will compare it to extreme numbers, such as 3, 6, 9, 12, 16.
+			horizontalIndent=horizontalIndent-cellSize;//Move horizontal indent position.
+			verticalIndent = 0;//...and top. Let`s move the position to the begin of the next columt.
+			$(currentCell).addClass("topOfTheRow");//Edds class to current element. That`s why we will be able to add display: inline-block; in .css for current class.
 		}
-		$(".block"+currentCell).css({'position': 'relative','top' : verticalIndent+"px",'left' : horizontalIndent+"px"});//Присвоить текущему элементу уникальную позицию.
-		//$(".block"+currentCell).offset({ top: getYPositionOfElement(), left: getXPositionOfElement() });
+		$(".block"+currentCell).css({'position': 'relative','top' : verticalIndent+"px",'left' : horizontalIndent+"px"});//Set the unique position to current element.
 		verticalIndent = verticalIndent-cellSize;
 	}
 	$("body").append("</div>");
@@ -44,6 +30,6 @@ var cellSize = 50, //Размер ячейки.
 	
 });
 
-    $(document).ready(function(){//Сделать все блоки перемещаемыми.
+    $(document).ready(function(){//Make all puzzle pieces draggable.
 	$( ".draggable" ).draggable();
     });
